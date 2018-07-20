f3617078acb72c7fd4a064d2ce994f09798e1c88
harnau <harnau@omatech.com>
2018-07-20 12:55:01 +0200

    command create editora

--- src/Omatech/Editora/Connector/Commands/EditoraCreate.php
+++ src/Omatech/Editora/Connector/Commands/EditoraCreate.php
@@ -41,7 +41,7 @@ class EditoraCreate extends Command
         $command = base_path('/vendor/omatech/editora-dbinterface/generate-editora.php');
         $arraydatabase = config_path('editoradatabase.php');
 
-        $this->line(shell_exec('php '.$command.' --from=file --inputformat=array --inputfile='.$arraydatabase.' --to=db4 --dbtohost='.env('DB_CONNECTION').' --dbtouser='.env('DB_USERNAME').' --dbtopass='.env('DB_PASSWORD').' --dbtoname='.env('DB_DATABASE').''));
+        $this->line(shell_exec('php '.$command.' --from=file --inputformat=array --inputfile='.$arraydatabase.' --to=db4 --dbtohost='.env('DB_HOST').' --dbtouser='.env('DB_USERNAME').' --dbtopass='.env('DB_PASSWORD').' --dbtoname='.env('DB_DATABASE').''));
     }
 
 }
