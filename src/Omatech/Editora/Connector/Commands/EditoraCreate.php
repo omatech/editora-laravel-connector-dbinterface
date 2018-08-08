d5b054c54dd8cacaf67430a7785eb6d8a20c6105
Alvaro Aguilar <aaguilar@omatech.com>
2018-08-08 10:37:12 +0200

    Editora Create: Route to file generator.php (Directory Commands)

--- src/Omatech/Editora/Connector/Commands/EditoraCreate.php
+++ src/Omatech/Editora/Connector/Commands/EditoraCreate.php
@@ -38,7 +38,7 @@ class EditoraCreate extends Command
      */
     public function handle()
     {
-        $command = base_path('/vendor/omatech/editora-dbinterface/generate-editora.php');
+        $command = base_path('/vendor/omatech/editora-dbinterface/Commands/generate-editora.php');
         $arraydatabase = config_path('editoradatabase.php');
 
         $this->line(shell_exec('php '.$command.' --from=file --inputformat=array --inputfile='.$arraydatabase.' --to=db4 --dbtohost='.env('DB_HOST').' --dbtouser='.env('DB_USERNAME').' --dbtopass='.env('DB_PASSWORD').' --dbtoname='.env('DB_DATABASE').''));
