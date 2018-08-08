24a0ebd89cb4268351bfc682730e2cd7efdbf0a2
Alvaro Aguilar <aaguilar@omatech.com>
2018-08-08 10:37:42 +0200

    Fake Content: Route to file fakecontent.php (Directory Commands) and arguments

--- src/Omatech/Editora/Connector/Commands/EditoraFakeContent.php
+++ src/Omatech/Editora/Connector/Commands/EditoraFakeContent.php
@@ -3,7 +3,8 @@
 namespace Omatech\Editora\Connector\Commands;
 
 use Illuminate\Console\Command;
-
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 
 class EditoraFakeContent extends Command
 {
@@ -12,7 +13,7 @@ class EditoraFakeContent extends Command
      *
      * @var string
      */
-    protected $signature = 'editora:fakecontent';
+    protected $signature = 'editora:fakecontent {--num_instances=} {--include_classes=} {--exclude_classes=} {--pictures_theme=} {--debug=}';
 
     /**
      * The console command description.
@@ -39,9 +40,27 @@ class EditoraFakeContent extends Command
     public function handle()
     {
         if(strcmp(env('APP_ENV'), 'local') == 0) {
+            $arguments = '';
+            if(!empty($this->option('num_instances') )){
+                $arguments .= " --num_instances=".$this->option('num_instances')." ";
+            }
+            if(!empty($this->option('include_classes'))){
+                $arguments .= " --include_classes=".$this->option('include_classes')." ";
+            }
+            if(!empty($this->option('exclude_classes'))){
+                $arguments .= " --exclude_classes=".$this->option('exclude_classes')." ";
+            }
+            if(!empty($this->option('pictures_theme'))){
+                $arguments .= " --pictures_theme=".$this->option('pictures_theme')." ";
+            }
+            if(!empty($this->option('debug'))){
+                $arguments .= " --debug=".$this->option('debug')." ";
+            }else{
+                $arguments .= " --debug=false ";
+            }
 
-            $command = base_path('/vendor/omatech/editora-dbinterface/fake-content.php');
-            $this->line(shell_exec('php '.$command.' --to=db4 --dbhost='.env('DB_CONNECTION').' --dbuser='.env('DB_USERNAME').' --dbpass='.env('DB_PASSWORD').' --dbname='.env('DB_DATABASE').''));
+            $command = base_path('/vendor/omatech/editora-dbinterface/Commands/fake-content.php');
+            $this->line(shell_exec('php '.$command.' --to=db4 --dbhost='.env('DB_CONNECTION').' --dbuser='.env('DB_USERNAME').' --dbpass='.env('DB_PASSWORD').' --dbname='.env('DB_DATABASE').' '.$arguments));
 
         }else{
             print_r('Function only local environment');
