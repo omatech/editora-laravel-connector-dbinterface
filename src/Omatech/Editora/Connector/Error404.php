c64e3dbb11c2582ff72168f0f7db6390f81bce8e
apons <apons@APONS-HP-EMO>
2018-08-13 14:44:12 +0200

    404

--- src/Omatech/Editora/Connector/Error404.php
+++ src/Omatech/Editora/Connector/Error404.php
@@ -4,24 +4,23 @@ namespace App\Http\Controllers\Editora;
 
 use App;
 //use App\Models\HomeModel;
-
 use Illuminate\Http\Request;
 use Omatech\Editora\Generator\Generator;
 use Omatech\Editora\Clear\Clear;
 
-class Error404 extends GlobalController{
-    public $inst_id;
-    public $preview;
-    public $viewData;
+class Error404 extends GlobalController {
 
+	public $inst_id;
+	public $preview;
+	public $viewData;
 
-    public function render(Request $request) {
-        $instance['id'] = 0;
-        $instance['link'] = $request->getRequestUri();
-        $this->viewData['instance'] = $instance;
+	public function render(Request $request) {
+		$this->viewData['global'] = $global = $request['global'];
+		$instance['id'] = 0;
+		$instance['link'] = $request->getRequestUri();
+		$this->viewData['instance'] = $instance;
 
-        return response()->view('editora.404', $this->viewData, 404);
-    }
-
+		return response()->view('editora.404', $this->viewData, 404);
+	}
 
 }
