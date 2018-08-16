867d1cae74883b984922108dcff75ef7e673bf36
Alvaro Aguilar <aaguilar@omatech.com>
2018-08-16 09:37:12 +0200

    pagination isset

--- src/Omatech/Editora/Connector/pagination_editora.blade.php
+++ src/Omatech/Editora/Connector/pagination_editora.blade.php
@@ -2,31 +2,33 @@
     <div class="container">
         <nav class="pagination">
 
-            @if ($paginator['onFirstPage'])
-                <a class="pager-arrow pager-prev"><span class="sr-only">{{_statictext('paginator.previous_page')}}</span></a>
-            @else
-                <a href="{{ $paginator['elements'][$paginator['previousPage']]['url'] }}" class="pager-arrow pager-prev"><span class="sr-only">{{_statictext('paginator.previous_page')}}</span></a>
-            @endif
+            @if(isset($paginator))
+                @if ($paginator['onFirstPage'])
+                    <a class="pager-arrow pager-prev"><span class="sr-only">{{_statictext('paginator.previous_page')}}</span></a>
+                @else
+                    <a href="{{ $paginator['elements'][$paginator['previousPage']]['url'] }}" class="pager-arrow pager-prev"><span class="sr-only">{{_statictext('paginator.previous_page')}}</span></a>
+                @endif
 
-            <ul class="pager-list">
-                @foreach ($paginator['elements'] as $page_num => $element)
-                    @if (is_string($element))
-                        <li class="disabled"><a href="#">{{ $element }}</a></li>
-                    @endif
-                    @if (is_array($element))
-                        @if ($element['isCurrent'])
-                            <li class="active">{{ $page_num }}</li>
-                        @else
-                            <li><a href="{{ $element['url'] }}">{{ $page_num }}</a></li>
+                <ul class="pager-list">
+                    @foreach ($paginator['elements'] as $page_num => $element)
+                        @if (is_string($element))
+                            <li class="disabled"><a href="#">{{ $element }}</a></li>
                         @endif
-                    @endif
-                @endforeach
-            </ul>
+                        @if (is_array($element))
+                            @if ($element['isCurrent'])
+                                <li class="active">{{ $page_num }}</li>
+                            @else
+                                <li><a href="{{ $element['url'] }}">{{ $page_num }}</a></li>
+                            @endif
+                        @endif
+                    @endforeach
+                </ul>
 
-            @if ($paginator['hasMorePages'])
-                <a href="{{ $paginator['elements'][$paginator['nextPage']]['url'] }}" class="pager-arrow pager-next"><span class="sr-only">{{_statictext('paginator.next_page')}}</span></a>
-            @else
-                <a class="pager-arrow pager-next"><span class="sr-only">{{_statictext('paginator.next_page')}}</span></a>
+                @if ($paginator['hasMorePages'])
+                    <a href="{{ $paginator['elements'][$paginator['nextPage']]['url'] }}" class="pager-arrow pager-next"><span class="sr-only">{{_statictext('paginator.next_page')}}</span></a>
+                @else
+                    <a class="pager-arrow pager-next"><span class="sr-only">{{_statictext('paginator.next_page')}}</span></a>
+                @endif
             @endif
         </nav>
     </div>
