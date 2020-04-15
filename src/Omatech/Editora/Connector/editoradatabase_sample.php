0bf068c948f441c44518ee71a6b2ae4d29ab3932
Harnau <harnau@omatech.com>
2020-04-15 09:23:56 +0200

    new editoradatabase sample for new generator

--- src/Omatech/Editora/Connector/editoradatabase_sample.php
+++ src/Omatech/Editora/Connector/editoradatabase_sample.php
@@ -1,173 +1,186 @@
 <?php
 
-$data = array(
+$data = [
+    'attributes_params' => true,
 	'truncate_users'=>false,
-	'users' => array(
+	'users' => [
 		// name, type, default lang, rol_id, O|U (Omatech-super-admin or normal user)
-		array('omatech', 'Omatech super-admin', 'ca', 1, 'O'),
-		array('test', 'Administrator', 'ca', 2, 'U')
-	),
-	'languages' => array(
+		['omatech', 'Omatech', 'ca', 1, 'O'],
+		['test', 'Administrator', 'ca', 2, 'U']
+    ],
+    'roles' => [
+        //['id' => 3, 'name' => 'testrole', 'classes' => '10,20,30'],
+    ],
+
+    'languages' => [
 		10000 => 'ca',
 		20000 => 'es',
-		30000 => 'en'
-	),
-	'groups' => array(
-		'Principal' => 1,
-		'Secundaris' => 2,
-		'Elements' => 3
-	),
-	'classes' => array(
-		'Principal' => [
-			1 => ['Global', 'Global'],
-			10 => ['Home', 'Home'],
-		],
-		'Secundaris' => [
-			20 => ['Pages', 'Pàgines', 'Páginas', 'Pages'],
-			21 => ['News', 'Noticies', 'Noticias', 'News'],
-			22 => ['People', 'Persones', 'Personas', 'People']	
-		],
-		'Elements' => [
-			30 => ['Blocks', 'Blocs', 'Bloques', 'Blocks'],
-			31 => ['Links', 'Links'],
-			32 => ['Docs', 'Docs']
-		]
-	),
-	'attributes_order_string' => array(
-		//id=>array(tag, caption_ca, caption_es, caption_en OR id=>tag
-		101 => array('surname', 'Cognoms', 'Apellidos', 'Surname')
-	),
-	'attributes_order_date' => array(
-		//id=>array(tag, caption_ca, caption_es, caption_en OR id=>tag
-		102 => array('date', 'Data noticia', 'Fecha noticia', 'News Date')
-	),	
-	'attributes_string' => array(
-		//id=>array(tag, caption_ca, caption_es, caption_en OR id=>tag
-		100 => array('nom', 'Nom', 'Nombre', 'Name')
-	),
-	'attributes_multi_lang_string' => array(
-		//id=>array(tag, caption_ca, caption_es, caption_en OR id=>tag
-		200 => ['title', 'Títol', 'Título', 'Title'],
-		201 => ['subtitle', 'Subtítol', 'Subtítulo', 'Subtitle']
-	),
-	'attributes_textarea' => array(
-	//id=>array(tag, caption_ca, caption_es, caption_en OR id=>tag
-	),
-	'attributes_text' => array(
-		//id=>array(tag, caption_ca, caption_es, caption_en OR id=>tag		
-	),
-	'attributes_date' => array(
-	),
+		30000 => 'en',
+	],
+	'groups' => [
+        1 => [
+                'Principal',
+                'caption'=>['Principal', 'Principal', 'Principal'],
+                'classes' => [
+                    1 => [
+                        'Global',
+                        'caption'=>['Global'],
+                        'attributes'=>['2,200'],
+                        'relations' => [
+                            1001  => ['main_menu', 'childs'=>'20,21,22', 'caption' =>['Menú principal ca', 'Menú principal', 'Main menu']],
+                        ],
+                        'editable'=>false
+                    ],
+                    10 => [
+                        'Home',
+                        'caption'=>['Home'],
+                        'attributes'=>['2,101,102,100,200,201,249,250,251,252,253,254,255,256,257,258,259,260,261,601,602,770'],
+                        'relations' => [
+                            10001 => ['home_blocks', 'childs'=>'30', 'caption' =>['Blocs destacats', 'Bloques destacados', 'Home blocks']],
+                            10002 => ['shortcuts', 'childs'=>'20', 'caption' =>['Accessos directes', 'Accesos directos', 'Shortcuts']],
+                            10003 => ['news_highlights', 'childs'=>'21', 'caption' =>['Notícies destacades', 'Notícias destacadas', 'News']],
+                        ],
+                        'editable'=>true
+                    ],
+                ],
+            ],
+		2 => [
+                'Secundaris',
+                'caption'=>['Secundaris', 'Secundarios', 'Secondary'],
+                'classes' => [
+                    20 => [
+                        'Pages',
+                        'caption'=>['Pàgines', 'Páginas', 'Pages'],
+                        'attributes'=>['2,200,201,600'],
+                        'relations' => [
+                            20001 => ['blocks', 'childs'=>'30', 'caption' =>['Bloques', 'Bloques', 'Blocks']],
+                        ],
+                    ],
+                    21 => [
+                        'News',
+                        'caption'=>['Noticies', 'Noticias', 'News'],
+                        'attributes'=>['2,200'],
+                        'relations' => [
+                            21001 => ['blocks', 'childs'=>'30', 'caption' =>['Bloques', 'Bloques', 'Blocks']],
+                            21002 => ['people', 'childs'=>'22', 'caption' =>['Bloques', 'Bloques', 'Blocks']],
+                        ],
+                    ],
+                    22 => [
+                        'People',
+                        'caption'=>['Persones', 'Personas', 'People'],
+                        'attributes'=>['2,200'],
+                        'relations' => [
+                            22001 => ['blocks', 'childs'=>'30', 'caption' =>['Bloques', 'Bloques', 'Blocks']],
+                        ],
+                    ]
+                ],
+            ],
+		3 => [
+                'Elements',
+                'caption'=>['Elements', 'Elementos', 'Elements'],
+                'classes' => [
+                    30 => [
+                        'Blocks',
+                        'caption'=>['Blocs', 'Bloques', 'Blocks'],
+                        'attributes'=>['201'],
+                        'relations' => [
+                            30001 => ['links', 'childs'=>'31', 'caption' =>['Bloques', 'Bloques', 'Blocks']],
+                            30002 => ['documents', 'childs'=>'32', 'caption' =>['Bloques', 'Bloques', 'Blocks']],
+                            30003 => ['people', 'childs'=>'22', 'caption' =>['Bloques', 'Bloques', 'Blocks']],
+                        ],
+                    ],
+                    31 => [
+                        'Links',
+                        'caption'=>['Links'],
+                        'attributes'=>['201'],
+                        'relations' => [
+                            31001 => ['internal_link', 'childs'=>'1,20,21,22', 'caption' =>['Bloques', 'Bloques', 'Blocks']],
+                        ],
+                    ],
+                    32 => [
+                        'Docs',
+                        'caption'=>['Docs'],
+                        'attributes'=>['201']
+                    ]
+                ]
+            ],
+	],
 
-	//selector de color
-	'attributes_color' => array(),
+	'attributes_order_string' => [
+		101 => ['surname', 'caption'=>['Cognoms', 'Apellidos', 'Surname']],
+	],
+	'attributes_order_date' => [
+		102 => ['order_date', 'caption'=>['Data noticia', 'Fecha noticia', 'News Date']],
+	],
+	'attributes_string' => [
+		100 => ['nom', 'caption'=>['Nom ', 'Nombre', 'Name']],
+	],
+	'attributes_multi_lang_string' => [
+		200 => ['title', 'caption'=>['Títol', 'Título', 'Title'], 'description'=>'una prueba de descripción'],
+		201 => ['subtitle', 'caption'=>['Subtítol', 'Subtítulo', 'Subtitle']],
+	],
+	'attributes_textarea' => [
+        249 => ['textarea', 'caption'=>['textarea ca', 'textarea es', 'textarea en']],
+    ],
+    'attributes_multi_lang_textarea' => [
+		250 => ['lang_textarea', 'caption'=>['lang_textarea ca', 'lang_textarea es', 'lang_textarea en']],
+	],
+	'attributes_text' => [
+		251 => ['text', 'caption'=>['text ca', 'text es', 'text en']],
+    ],
+    'attributes_multi_lang_text' => [
+		280 => ['lang_text', 'caption'=>['lang_text ca', 'lang_text es', 'lang_text en']],
+	],
+	'attributes_date' => [
+        252 => ['date', 'caption'=>['date ca', 'date es', 'date en']],
+	],
 
-	'attributes_num' => array(),
-	'attributes_geolocation' => array(),
-	'attributes_url' => array(
-		740 => 'url'
-	),
-	'attributes_file' => array(
-		700 => 'file'	
-	),
-	'attributes_video' => array(
-		760 => 'video'
-	),
-    'attributes_multi_lang_video' => array(),
-	'attributes_image' => array(
-		//id=>array(tag, caption_ca, caption_es, caption_en OR id=>tag
-		600 => ['header_picture', 'Imatge capçalera', 'Imágen cabecera', 'Header Picture'],
-		601 => ['profile_picture', 'Imatge perfil', 'Imágen perfil', 'Profile picture'],
-		602 => ['portrait_picture', 'Imatge retrat', 'Imágen retrato', 'Portrait picture'],
-		603 => ['block_image', 'Imatge bloc', 'Imágen Bloque', 'Block Picture']
-	),
-	'images_sizes' => array(
-		600 => '780x',
-		601 => '150x150',
-		602 => '300x',
-		603 => '400x'
-	),
-	'attributes_lookup' => array(
-		770 => ['image_position,70', 'Posició imatge', 'Posición imágen', 'Image position']
-	),
-	'lookups' => array(
-		'70,image_position' => [
-			7001 => ['left', 'Esquerra', 'Izquierda', 'Left'],
-			7002 => ['right', 'Dreta', 'Derecha', 'Right']
-		]
-	),
+	'attributes_color' => [
+        253 => ['color', 'caption'=>['color ca', 'color es', 'color en']],
+    ],
 
-	'attributes_multi_lang_url' => array(
-	),
-	'attributes_multi_lang_image' => array(
-		//id=>array(tag, caption_ca, caption_es, caption_en OR id=>tag		
-	),
-	'attributes_multi_lang_textarea' => array(
-		//id=>array(tag, caption_ca, caption_es, caption_en OR id=>tag
-		400 => 'text',
-		401 => 'intro',
-		402 => 'bio',
-		403 => 'quote'
-	),
-	'attributes_multi_lang_file' => array(
-	),
+	'attributes_num' => [
+        254 => ['num', 'caption'=>['num ca', 'num es', 'num en']],
+    ],
+	'attributes_geolocation' => [
+        255=> ['geolocation', 'caption'=>['geolocation ca', 'geolocation es', 'geolocation en']],
+    ],
+	'attributes_url' => [
+        256=> ['url', 'caption'=>['url ca', 'url es', 'url en']],
+    ],
+    'attributes_multi_lang_url' => [
+        257=> ['lang_url', 'caption'=>['lang_url ca', 'lang_url es', 'lang_url en']],
+	],
+	'attributes_file' => [
+        258=> ['file', 'caption'=>['file ca', 'file es', 'file en']],
+    ],
+    'attributes_multi_lang_file' => [
+        259=> ['lang_file', 'caption'=>['lang_file ca', 'lang_file es', 'lang_file en']],
+    ],
+	'attributes_video' => [
+		260=> ['video', 'caption'=>['video ca', 'video es', 'video en']],
+	],
+    'attributes_multi_lang_video' => [
+        261=> ['lang_video', 'caption'=>['lang_video ca', 'lang_video es', 'lang_video en']],
+    ],
+    'attributes_image' => [
+        601 => ['profile_picture', 'caption' =>['Imatge perfil', 'Imágen perfil', 'Profile picture'], 'params'=>['size'=>['150x200']]],
+    ],
+    'attributes_multi_lang_image' => [
+        602 => ['lang_picture', 'caption' =>['Imatge ', 'Imágen ', 'Profile '], 'params'=>['size'=>['700x200']]],
+    ],
+    'attributes_grid_image' => [
+        603 => ['grid_image', 'caption' =>['grid_image ca', 'grid_image es', 'grid_image en'], 'params'=>['size'=>['700x200']]],
+    ],
+	'attributes_lookup' => [
+        770 => ['image_position', 'caption' =>['Posició imatge', 'Posición imágen', 'Image position'],
+                'params'=>['lookup'=>[
+                    7001 => ['left', 'Esquerra', 'Izquierda', 'Left'],
+                    7002 => ['right', 'Dreta', 'Derecha', 'Right']
+                ]]]
+	],
 
+];
 
-	'relations' => array(
-		1001 => '1,20,21,22',
-		10001 => '10,30',
-		10002 => '10,20',
-		10003 => '10,21',
-		
-		20001 => '20,30',
-		
-		21001 => '21,30',
-		21002 => '21,22',
-		
-		22001 => '22,30',
-
-		30001 => '30,31',
-		30002 => '30,32',
-		30003 => '30,22',
-		
-		31001 => '31,1,20,21,22'
-		
-	),
-	'relation_names' => array(
-		1001 => ['Menú principal', 'main_menu'],
-		10001 => ['Blocs destacats', 'home_blocks'],
-		10002 => ['Accessos directes', 'shortcuts'],
-		10003 => ['Notícies destacades', 'news_highlights'],
-		
-		20001 => ['Blocks', 'blocks'],
-		
-		21001 => ['Blocks', 'blocks'],
-		21002 => ['People', 'people'],
-		
-		22001 => ['Blocks', 'blocks'],
-		
-		30001 => ['Links','links'],
-		30002 => ['Documents','documents'],
-		30003 => ['People','people'],
-		
-		31001 => ['InternalLink','internal_link']
-
-	),
-	// attribute=2 nice_url
-	// class_id 1=Home and 10=Global
-	'attributes_classes' => array(
-		1 => '2,200',
-		10 => '2,200',
-		20 => '2,200,201,600,401,400',
-		21 => '2,201,202,102,600,401,400',
-		22 => '2,100,101,402,403,601,602',
-		30 => '201,202,400,403,70,603,700',
-		31 => '201,740',
-		32 => '201,700'
-	),
-	'roles' => array(
-	//array('id' => 3, 'name' => 'testrole', 'classes' => '10,20,30'),
-	)
-);
 
 
