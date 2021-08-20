efd61328d28e03cf84d7d4b58cf46765d57673fc
Agusti Pons <apons@omatech.com>
2021-08-20 13:42:54 +0200

    bug in check

--- src/Omatech/Editora/Connector/Directives/GenerateEditLinkDirective.php
+++ src/Omatech/Editora/Connector/Directives/GenerateEditLinkDirective.php
@@ -1,15 +1,14 @@
 <?php
     Blade::directive('generate_edit_link', function ($instance) {
-        if (!isset($instance['metadata']['id']) || !isset($instance['metadata']['class_id'])) {
-            return '';
-        }
-        
         $instanceId = $instance."['metadata']['id']";
         $classId = $instance."['metadata']['class_id']";
         $php = "<?php
+          if (isset($instanceId) && isset($classId))
+          {
             if(session()->has('user_id') && session()->get('user_id') !== '') {
                 echo '<a href=\"/'.config('editora.adminAlias').'/view_instance?p_class_id='.{$classId}.'&p_inst_id='.{$instanceId}.'\" class=\"front-edit\" title=\"Editar\" target=\"_blank\"  style=\"display: none;\"><img class=\"edit_img\" src=\"/images/editar.png\" alt=\"Editar\"/></a>';
             }
+          }
         ?>";
 
         return $php;
